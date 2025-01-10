29d28f88c7c47b832dd94c464aa9a47633e065d0
SharafatKarim <sharafat2004@gmail.com>
2025-01-10 10:27:56 +0600

    backend URL -> frontend :: updated 6

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:5001/api/",
-  baseURL: import.meta.env.mode === "production" ?  "/api/" : "http://localhost:5001/api/",
+  // baseURL: import.meta.env.mode === "production" ?  "/api/" : "http://localhost:5001/api/",
+  baseURL: "/api/",
   withCredentials: true,
   timeout: 5000
 });
